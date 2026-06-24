2a347f870109e9cf7e68d9c2b08c638e17a50904
Lê Đại Thông <thongledai@gmail.com>
2026-06-24 18:21:57 +0700

    fix conflict

--- Algorithms/UninformedSearch/UniformCostSearch.py
+++ Algorithms/UninformedSearch/UniformCostSearch.py
@@ -1,4 +1,4 @@
-import heapq
+from collections import deque
 import time
 from Core.Node import *
 from Core.Action import *
@@ -6,55 +6,28 @@ from Core.Utils import *
 from Core.Result import *
 
 
-# Thuật toán tìm kiếm chi phí đồng nhất
-def UCS(START):
+# Thuật toán tìm kiếm theo chiều rộng
+def BFS(START):
     start_time=time.time()
     node=Node(START)
     if is_goal(node.state):
-        return Solution(
-            success=True,
-            path=node.get_path(),
-            states=node.get_states(),
-            last_state=node.get_state(),
-            cost=node.get_cost(),
-            generated_states=1,
-            depth=node.get_depth(),
-            runtime=time.time()-start_time
-        )
+        return Solution(True, node.get_path(), node.get_states(), node.get_state(), node.get_cost(), 1, node.get_depth(), time.time()-start_time)
     
-    frontier = []
+    frontier = deque([node])
     explored = []
-    heapq.heappush(frontier, node)
-
+    
     while frontier:
-        node = heapq.heappop(frontier)
+        node = frontier.popleft()
         explored.append(node.state)
-        if is_goal(node.state):
-            return Solution(
-                success=True,
-                path=child.get_path(),
-                states=child.get_states(),
-                last_state=child.get_state(),
-                cost=child.get_cost(),
-                generated_states=len(explored) + len(frontier),
-                depth=child.get_depth(),
-                runtime=time.time()-start_time
-            )
-        
+
         actions = get_actions(node.state)
-        for action1 in actions:
-            child = node.expand(action=action1,cost_function="g(x)")
+        for action in actions:
+            child = node.expand(action)
+
             if child.state not in explored and child not in frontier:
-                heapq.heappush(frontier, child)
+                if is_goal(child.state):
+                    return Solution(True, child.get_path(), child.get_states(), child.get_state(), child.get_cost(), len(explored) + len(frontier), child.get_depth(), time.time()-start_time)
+                frontier.append(child)
 
     # failure
-    return Solution(
-        success=False,
-        path=node.get_path(),
-        states=node.get_states(),
-        last_state=node.get_state(),
-        cost=node.get_cost(),
-        generated_states=len(explored) + len(frontier),
-        depth=node.get_depth(),
-        runtime=time.time()-start_time
-    )  
+    return Solution(False, node.get_path(), node.get_states(), node.get_state(), node.get_cost(), len(explored) + len(frontier), node.get_depth(), time.time()-start_time)
