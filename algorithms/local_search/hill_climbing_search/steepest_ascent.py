248d62fd6e9028938b60a24068e7b85f61da5102
Lê Đại Thông <thongledai@gmail.com>
2026-06-30 07:29:01 +0700

    Refactor steepest ascent search to allow revisiting nodes
    Removed the visited set to allow revisiting nodes.

--- algorithms/local_search/hill_climbing_search/steepest_ascent.py
+++ algorithms/local_search/hill_climbing_search/steepest_ascent.py
@@ -10,7 +10,6 @@ def steepest_ascent_hill_climbing_search(START):
     current = Node(START, cost=heuristic(START))
     explored_nodes = 0
     generated_nodes = 1
-    visited = {state_to_tuple(START)}
 
     for _ in range(MAX_STEPS):
         if is_goal(current.state):
@@ -20,13 +19,12 @@ def steepest_ascent_hill_climbing_search(START):
         children = child_nodes(current)
         generated_nodes += len(children)
         
-        valid_children = [c for c in children if state_to_tuple(c.state) not in visited]
+        valid_children = [c for c in children]
         candidate = min(valid_children, key=lambda child: child.cost) if valid_children else None
         
         if (candidate is None) or (candidate.get_cost() > current.get_cost()):
             break
 
         current = candidate
-        visited.add(state_to_tuple(current.state))
 
     return solution(current, explored_nodes, generated_nodes, start_time)
